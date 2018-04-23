cce49c3700ec18d0f06d07258c1f022b6ccff249
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to sort/group data in a GridColumn against data in another GridColumn
+
+
+<p><strong>Update: </strong>Starting with 17.1, it is enough to use the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfGridColumnBase_SortFieldNametopic">GridColumn.SortFieldName</a> property to get the desired results.<br><br>For older versions, select an appropriate solution in the ComboBox below.</p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports DevExpress.Data.Linq
+Imports System.Collections.Generic
+Imports DevExpress.Data
+Imports System.ComponentModel
+
+Namespace DXGrid_EF4_ServerMode
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			'You can use this demo code to generate a large data set, if currently, you don't have a large database for testing.
+			Dim dataContext As New NorthwindEntities()
+
+			Dim source As New LinqServerModeSource() With {.ElementType = GetType(Product), .KeyExpression = "ProductID", .QueryableSource = dataContext.Products}
+
+			grid.DataSource = source
+			grid.Columns.Remove(grid.Columns("SupplierID"))
+			grid.Columns("ProductName").SortIndex = 0
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.AutoPopulateColumns = False
+			Dim replacements As New Dictionary(Of String, String)()
+			replacements.Add("ProductName", "SupplierID")
+			grid.DataSource = New MyListSource(TryCast(grid.DataSource, IListSource), replacements)
+
+		End Sub
+	End Class
+End Namespace
